44a7f1f497735d84616305aeca85b14eb19ca00a
dathaquoc <dat.haquoc@ncc.asia>
2025-09-15 17:16:55 +0700

    change logic prepare state

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     timeout: 10 * 1000,
   },
   forbidOnly: !!process.env.CI,
-  retries: 3,
+  retries: process.env.CI ? 3 : 0,
   fullyParallel: false,
   workers,
   reporter: [
