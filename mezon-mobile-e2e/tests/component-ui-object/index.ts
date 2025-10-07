706301ae483aef603f7c5c4cd24dd63eabc1c6e7
minh.do <minh.dovan@ncc.asia>
2025-10-07 17:05:02 +0700

    task-64074: update flow login on mobile

--- mezon-mobile-e2e/tests/component-ui-object/index.ts
+++ mezon-mobile-e2e/tests/component-ui-object/index.ts
@@ -3,3 +3,4 @@ export * from "./mezon-button/index.js";
 export * from "./mezon-button-icon/index.js";
 export * from "./mezon-menu/index.js";
 export * from "./mezon-switch/index.js";
+export * from "./mezon-confirm/index.js";
