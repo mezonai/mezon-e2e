735fb03351f202ea9df08ae5235f3245367d6c53
trinh.truongthiphuong <trinh.truongthiphuong@ncc.asia>
2026-07-22 15:48:34 +0700

    update test match

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,16 +24,16 @@ const getInsecureLocalOriginArgs = (): string[] => {
 
 export default defineConfig({
   testDir: './src/tests',
-  // testMatch: [
-  //   '**/cleanup/**/*.spec.ts',
-  //   '**/multi-chat/ChannelMessage/**/*.spec.ts',
-  //   '**/multi-chat/ClanManagement/**/*.spec.ts',
-  //   '**/multi-chat/FriendManagement/**/*.spec.ts',
-  //   '**/web/ChannelManagement/**/*.spec.ts',
-  //   '**/web/ChannelMessage/**/*.spec.ts',
-  //   '**/web/ClanManagement/**/*.spec.ts',
-  //   '**/web/ThreadManagement/**/*.spec.ts',
-  // ],
+  testMatch: [
+    '**/cleanup/**/*.spec.ts',
+    '**/multi-chat/ChannelMessage/**/*.spec.ts',
+    '**/multi-chat/ClanManagement/**/*.spec.ts',
+    '**/multi-chat/FriendManagement/**/*.spec.ts',
+    '**/web/ChannelManagement/**/*.spec.ts',
+    '**/web/ChannelMessage/**/*.spec.ts',
+    '**/web/ClanManagement/**/*.spec.ts',
+    '**/web/ThreadManagement/**/*.spec.ts',
+  ],
   grepInvert: /@dual/,
   timeout: 300 * 1000,
   expect: {
