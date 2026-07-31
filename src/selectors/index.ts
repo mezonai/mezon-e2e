bb92c5ef515e02e74ec40a2dbdf607de8066ba70
trinh.truongthiphuong <trinh.truongthiphuong@ncc.asia>
2026-07-31 11:33:03 +0700

    add test case verify message on inbox tab

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -825,7 +825,14 @@ export const DATA_E2E_IDENTIFIER = {
           },
         },
         message: '',
-        for_you: '',
+        for_you: {
+          username: '',
+          message: '',
+          timestamp: '',
+          button: {
+            remove: '',
+          },
+        },
         action_tabs: '',
       },
     },
